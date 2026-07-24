cc642c5bcae6bec9115cc8494718696f0c45e273
raniaamir4110 <raniaamir729926@gmail.com>
2026-07-25 03:49:32 +0500

    2 chaps done

---
+++ javascript chap 1/app.js
@@ -0,0 +1,15 @@
+// Question no 1
+// alert("Hello! Welcome to my website")
+
+// Question no 2
+// alert("Error! Please enter a valid password")
+
+// Question no 3
+// alert("Welcome to JS Land...\nHappy Coding")
+
+// Question no 4
+// alert("Welcome to JS Land")
+// alert("Happy Coding")
+
+// Question no 5
+alert("Hello... I can run JS through my web browser's console")
---
+++ javascript chap 2/app.js
@@ -0,0 +1,35 @@
+// Question no 1
+// var username = "raniaamir"
+
+// Question no 2
+// var myName="Rania Amir"
+// document.write(myName)
+
+// Question no 3
+// var message="Hello World"
+// alert(message)
+
+// Question no 4
+// var name=("Jhone Doe")
+// var age=("15")
+// var skills=("Certified Mobile Application Development")
+
+// alert(name)
+// alert(age)
+// alert(skills)
+
+// Question no 5
+// var food=("PIZZA\nPIZZ\nPIZ\nPI\nP")
+// alert(food)
+
+// Question no 6
+// var email=("raniaamir729926@gmail.com")
+// alert("My email address is " + email)
+
+// Question no 7
+// var a_smarter_way_to_learn_javascript=("I am trying to learn from the Book A smarter way to Learn JavaScript")
+// alert(a_smarter_way_to_learn_javascript)
+
+// Question no 8
+var msg=("Yah! I can write HTML content through JavaScript")
+document.write(msg)
