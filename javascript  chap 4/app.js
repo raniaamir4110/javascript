50f6b204f551f88b41f019339c33fa811fa59a0d
raniaamir4110 <raniaamir729926@gmail.com>
2026-07-28 00:04:16 +0500

    done

---
+++ javascript  chap 4/app.js
@@ -0,0 +1,21 @@
+// Question no 1
+// let  a, b, c;
+
+// Question no 2
+// let myName, nice112, userAlert, good$, rania_amir;
+// let hello 5, nice#22, yahoo!,  666bye, rania aa;
+
+// Question no 3
+document.write("Rules for naming JS variables")
+document.write("<br>")
+document.write("<br>")
+
+let num="numbers", dollar="$", under="_", dot=".";
+let alpha="letter";
+let word="sensitive";
+let kword="keywords";
+
+document.write("Variable names can only contain " + num + ", " + dollar + ", " + under + "and" + dot + "For example :$my_1stVariable")
+document.write("<br>Variables must begin with a " + alpha + ", " + dollar + " or " + under + dot + "  For example $name, _name or name")
+document.write("<br>Variable names are case " + word)
+document.write("<br>Variable names should not be JS " + kword)
---
+++ javascript chap 5/app.js
@@ -0,0 +1,78 @@
+// Question no 1
+// var num1=3;
+// var num2=5;
+// var total1=num1+num2;
+// document.write("Sum of " + num1 + " and " + num2 + " is " + total1)
+
+// Question no 2
+// var total2=num1-num2;
+// document.write("<br>Subtraction of " + num1 + " and " + num2 + " is " + total2)
+
+// var total3=num1*num2;
+// document.write("<br>Multiplication of " + num1 + " and " + num2 + " is " + total3)
+
+// var total4=num1/num2;
+// document.write("<br>Division of " + num1 + " and " + num2 + " is " + total4)
+
+// var total5=num1%num2;
+// document.write("<br>modulus of " + num1 + " and " + num2 + " is " + total5)
+
+// Question no 3
+// var num1;
+// document.write("Value after variable declaration is: ??")
+// var num1=5;
+// document.write("<br>Initial value: 5")
+// num1++;
+// document.write("<br>Value after increment is : " + num1)
+//  num1=num1+7;
+// document.write("<br>Value after addition is : " + num1)
+// num1--;
+// document.write("<br>value after decrement is :" + num1)
+// num1=num1/3;
+// document.write("<br>The reminder is : " + num1)
+
+// Question no 4
+// var cost=600, people=5;
+// var total=cost*people;
+// document.write("Total cost to buy " + people + " tickets to a movie is " + total + "PKR")
+
+// Question no 5
+// var num1=4, total=4, num2=1;
+// document.write("TABLE OF 4")
+// document.write("<br>" + num1 + "x" + num2 + "=" + total)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+// num2++;  
+// grandtotal=total*num2;
+// document.write("<br>" + num1 + "x" + num2 + "=" + grandtotal)
+
+// Question no 6
+var celsius=25;
+var Farenheit=(celsius*9/5)+32;
+document.write(celsius + "°C" + " in Farenheit is " + Farenheit + "°F")
+
+var Farenheit2=70
+var celsius2=(Farenheit2-32)*5/9;
+document.write("<br>" + Farenheit2 + "°F" + " in Celcius is " + celsius2 + "°C")
