15fe9f893bb2e13943fdbef09d0f984738aac9be
raniaamir4110 <raniaamir729926@gmail.com>
2026-08-04 00:14:11 +0500

    chap 9-11

---
+++ javascript chap 9-11/app.js
@@ -0,0 +1,111 @@
+// Question no 1
+// var city = prompt("Type A City Name")
+// if(city === "karachi"){
+//     alert("Welcome to THE CITY OF LIGHTS")
+// }
+
+// Question no 2
+// var gender = prompt("Type Your Gender")
+// if(gender === "male"){
+//     alert("GOOD MORNING! SIR")
+// }
+// else{
+//     alert("GOOD MORNING! MA'AM")
+// }
+
+// Question no 3
+// var color = prompt("Type any color of Traffic Signal")
+// if(color === "red"){
+//     alert("Must Stop")
+// } else if(color === "yellow"){
+//     alert("Ready to Move")
+// } else if(color === "green")
+// {
+//     alert("Move Now")
+// }
+
+// Question no 4
+// var fuel = prompt("How much fuel is remain in your car")
+// if(fuel <= 0.25){
+//     alert("Please Refill the fuel in your car")
+// } else{
+//     alert("GOOD! Have a nice trip")
+// }
+
+// Question no 5
+// var a = 4;
+// if (++a === 5){
+// alert("given condition for variable a is true");
+// }
+
+// var b = 82;
+// if (b++ === 83){
+// alert("given condition for variable b is true");
+// }
+
+// var c = 12;
+// if (c++ === 13){
+// alert("condition 1 is true");
+// }
+// if (c === 13){
+// alert("condition 2 is true");
+// }
+// if (++c < 14){
+// alert("condition 3 is true");
+// }
+// if(c === 14){
+// alert("condition 4 is true");
+// }
+
+// var materialCost = 20000;
+// var laborCost = 2000;
+// var totalCost = materialCost + laborCost;
+// if (totalCost === laborCost + materialCost){
+// alert("The cost equals");
+// }
+
+// if (true){
+// alert("True");
+// }
+// if (false){
+// alert("False");
+// }
+
+// if("car" < "cat"){
+// alert("car is smaller than cat");
+// }
+
+// Question no 6
+// var comp = Number(prompt("Type your Marks Obtained in Computer"))
+// var english = Number(prompt("Type your Marks Obtained in English"))
+// var math = Number(prompt("Type your Marks Obtained in Mathematics"))
+// var total = 300, marks_obtained = comp+english+math, percentage = marks_obtained/total*100;
+
+// document.write("MARKS SHEET")
+// document.write("<br>Total Marks : 300")
+// document.write("<br>Obtained Marks : " + marks_obtained)
+// document.write("<br>Percentage : " + percentage)
+// if(percentage >= 80){
+//     document.write("<br>Grade : A-one")
+//     document.write("<br>Remarks : Excellent")
+// } else if(percentage >= 70){
+//     document.write("<br>Grade : A")
+//     document.write("<br>Remarks : Good")
+// } else if(percentage >= 60){
+//     document.write("<br>Grade : B")
+//     document.write("<br>Remarks : You Need to improve")
+// } else if(percentage <= 60){
+//     document.write("<br>Grade : fail")
+//     document.write("<br>Remarks : Sorry")
+// } 
+
+// Question no 7
+// var secret_nmber = 5;
+// var guess = Number(prompt("GUESS THESECRET NUMBER"))
+// if(guess === secret_nmber){
+//     alert("BINGO! That's Correct")
+// }else if(++guess === secret_nmber){
+//     alert("Ooh! That's close")
+// }else{
+//     alert("Wrong! Try Next Time")
+// }
