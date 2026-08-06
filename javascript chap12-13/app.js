a654ecc9cf6d50f9faca46799076324574445571
raniaamir4110 <raniaamir729926@gmail.com>
2026-08-06 23:50:27 +0500

    chap 12-13 comp

---
+++ javascript chap12-13/app.js
@@ -0,0 +1,85 @@
+// Question no 1
+// var input = prompt("Type a Character. It can be a number or a letter,whether in lowercase or uppercase ")
+// var asciiCode = input.charCodeAt()
+// if (asciiCode>=65 && asciiCode <= 90){
+//     alert("It is an Uppercase letter")
+// }else if(asciiCode>=97 && asciiCode<=122){
+//     alert("It is a lowercase letter")
+// }else if(asciiCode>=48 && asciiCode<=57){
+//     alert("The input is a Number")
+// }
+
+
+// Question no 2
+// var int1 = prompt("Type an Integar")
+// var int2 = prompt("Type an Integar")
+// if(int1>int2){
+//     alert("this is larger " + int1)
+// }else if(int2>int1){
+//     alert(int2)
+// }else if(int1==int2 || int2==int1){
+//     alert("They are equal")
+// }
+
+
+// Question no 3
+// var input = +prompt("Type a Number")
+// if (input <= 0) {
+//     alert("Number is Negative")
+// } else if (input >= 0){
+//     alert("Number is Positive")
+// } else (input = 0) =>{
+//     alert("Number is Zero")
+// }
+
+
+// Question no 4 
+// var character = prompt("Type a character tocheck whether it's a vowel or not")
+// if(character == "a" || character == "A" || character == "e" || character == "E" || character == "i" || character == "I" ||character == "o" || character == "O" || character == "u" || character == "U"){
+//     alert("It's a vowel")
+// } else{
+//     alert("It's not a vowel!")
+// }
+
+
+// Question no 5
+// var password = prompt("ENTER YOUR PASSWORDS. (Hint : a for ??)")
+// var real_passwords = "apple"
+// if (password==real_passwords) {
+//     alert("Password is correct")
+// }else if (password.length=="") {
+//     alert("Please Enter your Password")
+// }else{
+//     alert("Incorrect Password!")
+// }
+
+
+// Question no 6
+// Provided Script
+// var greeting;
+// var hour = 13;
+// if(hour<18){
+//     greeting="Good day!";
+// }else {
+//     greeting="Good Evening";
+// }
+// corrected script
+// var hour = 12;
+
+// if (hour<12) {
+//     alert("Good Day!")
+// }else{
+//     alert("Good Evening!")
+// }
+
+// Question no 7
+// var time=prompt("What time is it?")
+// if (time>= 0000 && time <=1200) {
+//     alert("Good Morning")
+// }else if (time>1200 && time<1700) {
+//     alert("Good Afternoon")
+// }else if(time>1700 && time<2100){
+//     alert("Good Evening")
+// }else if(time>2100 && time<2359){
+//     alert("Good Evening")
+// }
