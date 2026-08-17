758d01e84661f72d980e65feb04c504d7322ba78
raniaamir4110 <raniaamir729926@gmail.com>
2026-08-17 21:29:21 +0500

    done

---
+++ javascript  chap 14-16/app.js
@@ -0,0 +1,86 @@
+// Question no 1
+// var student_name = [""]
+
+// Question no 2
+// var student_name2 = {}
+
+// Question no 3
+// var fruits_name = ["apple" , "banana" , "mango" , "cherry" , "pineapple" , "pear"]
+
+// Question no 4
+// var number = [12,4,44,66,89,44,2245]
+
+// Question no 5
+// var boolean = [true,false]
+
+// Quuestion no 6
+// var mixed_array = [0,true,"bob"]
+
+// Question no 7 
+// var qualification_in_pak = ["SSC","HSC","BCS","BS","MS","PhD","B.Com","M.Phil"]
+
+// Question no 8
+// var student_marks = ["Michael","John","Tony"]
+// var marks = [320,230,480]
+
+// var Percentage_michael = marks[0]/500*100
+// document.write(student_marks[0] + "'s marks are " + marks[0] + ".Percentage is " + Percentage_michael + "%")
+
+// var Percentage_john = marks[1]/500*100
+// document.write("<br>"+ student_marks[1] + "'s marks are " + marks[1] + ".Percentage is " + Percentage_john + "%")
+
+// var Percentage_tony = marks[2]/500*100
+// document.write("<br>"+ student_marks[2] + "'s marks are " + marks[2] + ".Percentage is " + Percentage_tony + "%")
+
+// Question no 9
+// var colors = ["blue","purple","green","orange","red"]
+// document.write("the array is "+ colors)
+// var user_color = prompt("Which color you want to add in the start of array")
+// colors.unshift(user_color)
+// document.write("<br>the updated array is "+ colors)
+
+// var user_color2 = prompt("Which color you want to add in the end of array")
+// colors.push(user_color2)
+// document.write("<br>the updated array is "+ colors)
+
+// colors.unshift("teal", "seagreen")
+// document.write("<br>the updated array is "+ colors)
+
+// colors.shift()
+// document.write("<br>the updated array is "+ colors)
+
+// colors.pop()
+// document.write("<br>the updated array is "+ colors)
+
+// var index_number = +prompt("In which index you want to add the color")
+// var index_color = prompt("which color you want to add")
+// colors.splice(index_number,index_number,index_color)
+// document.write("<br>the updated array is "+ colors)
+
+// Question no 10
+// var mixed_numbers = [320,230,480,120]
+// document.write("Scores of Students : " + mixed_numbers)
+// mixed_numbers.sort()
+// document.write("<br>Ordered Scores of Student : " + mixed_numbers)
+
+// Question no 11
+// var city_names = ["Karachi","Lahore","Islamabad","Quetta","Peshawar"]
+// var Selected_cities = [city_names[2],city_names[3]]
+// document.write("Cities List :")
+// document.write("<br>"+city_names)
+// document.write("<br><br>Selected Cities :")
+// document.write("<br>"+Selected_cities)
+
+// Question no 12
+// var array = ["My","name","is","Rania"]
+
+// document.write("Array: " + array)
+// var String = array.join()
+// document.write("<br>String: " + String)
+
+// Question no 13
+// var devices = ["keyboard","mouse","printer","monitor"]
+// document.write("Devices :" + "<br>" + devices)
+// document.write("<")
+
+// Question no 14
