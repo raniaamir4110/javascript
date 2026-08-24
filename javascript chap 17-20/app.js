4081767c6ffa56da81c8901692518a9cc338865e
raniaamir4110 <raniaamir729926@gmail.com>
2026-08-25 03:13:58 +0500

    Question 5 complete

---
+++ javascript chap 17-20/app.js
@@ -0,0 +1,34 @@
+// Question no 1
+// var  multi_array = [[[[[]]]]]
+
+// Question no 2
+// var matrix = [[0,1,2,3],[1,0,1,2],[2,1,0,1]]
+// console.log(matrix)
+
+// Question no 3
+// for(var i = 1; i<=10; i++){
+//     console.log(i)
+// }
+
+// Question no 4
+// var table = +prompt("Enter a number to show it's multiplication table")
+// var length = +prompt("Enter length of multiplication table")
+// for(var i = 1; i<=length; i++){
+//     document.write("<br>"+ table + "x" + i + "=" + table*i)
+// }
+
+// Question no 5
+// var fruits = ["apple","banana","mango","orange","strawberry"]
+// for(var i = 1; i<=fruits.length; i++){
+//     document.write("<br>" + fruits[0])
+// }
+
+// Question no 6
+document.write("counting : ")
+for(var i = 1; i<=15; i++){
+    document.write(i)
+}
+document.write("<br>reverse counting : ")
+for(var i = 10; i<=1; i--){
+    document.write(i)
+}
