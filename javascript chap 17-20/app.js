14ced51601ede11c31c07b592137373a9451ae8d
raniaamir4110 <raniaamir729926@gmail.com>
2026-09-03 00:30:49 +0500

    done

--- javascript chap 17-20/app.js
+++ javascript chap 17-20/app.js
@@ -28,14 +28,56 @@
 
 
 // Question no 6
-var forward_counting = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
-var reverse_counting = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
-var even_counting = [ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
-var odd_counting = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
-var series_counting = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
+// var forward_counting = [ 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
+// var reverse_counting = [10, 9, 8, 7, 6, 5, 4, 3, 2, 1]
+// var even_counting = [ 0, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20]
+// var odd_counting = [ 1, 3, 5, 7, 9, 11, 13, 15, 17, 19]
+// var series_counting = ["2k", "4k", "6k", "8k", "10k", "12k", "14k", "16k", "18k", "20k"]
 
-document.write("COUNTING : <br>" + forward_counting)
-document.write("<br><br>REVERSE COUNTING : <br>" + reverse_counting)
-document.write("<br><br>EVEN  : <br>" + even_counting)
-document.write("<br><br>ODD : <br>" + odd_counting)
-document.write("<br><br>SERIES : <br>" + series_counting)
+// document.write("COUNTING : <br>" + forward_counting)
+// document.write("<br><br>REVERSE COUNTING : <br>" + reverse_counting)
+// document.write("<br><br>EVEN  : <br>" + even_counting)
+// document.write("<br><br>ODD : <br>" + odd_counting)
+// document.write("<br><br>SERIES : <br>" + series_counting)
+
+// Question no 7
+// var A = ["cake","apple pie","cookie","chips","patties"]
+// var user_input = prompt("Welcome to ABC Bakery! What do you want to order Sir/Ma'am")
+// var index = A.indexOf(user_input)
+// if( index !== -1){
+//     alert(user_input +  " is available at index " + index + " in our bakery")
+// }else{
+//     alert("We're sorry " + user_input + " is not available in our bakery")
+// }
+
+// Question no 8
+// var A =[24, 53, 78, 91, 12]
+// document.write(A)
+// var largest_num = A[0]
+
+// for(var i = 0; i<A.length; i++){
+//     if(A[i] > largest_num){
+//         largest_num = A[i]
+//     }
+// }
+// document.write("The largest number is: " + largest_num)
+
+// Question no 9
+// var A =[24, 53, 78, 91, 12]
+// document.write(A)
+// var smallest_num = A[0]
+
+// for(var i = 0; i<A.length; i++){
+//     if(A[i] < smallest_num){
+//         smallest_num = A[i]
+//     }
+// }
+// document.write("The smallest number is: " + smallest_num)
+
+// Question no  10
+
+// for(var i=1; i<=100; i++){
+//     if(i % 5 === 0){
+//         document.write(i + "<br>")
+//     }
+// }
---
+++ javascript chap 21-25/app.js
@@ -0,0 +1,61 @@
+// Question no 1
+// var first_name = prompt("Enter your First Name")
+// var last_name = prompt("Enter your Last Name")
+// first_name = first_name.toUpperCase();
+// last_name = last_name.toUpperCase();
+
+// var  full_name = first_name+" "+last_name
+// alert("HI! " + full_name)
+
+// Question no 2
+// var phone_input = prompt("Type Name of your favourite phone model")
+// var stringlength = phone_input.length;
+
+// document.write("My Favourite Phone is : " + phone_input)
+// document.write("<br>Length of String : " + stringlength)
+
+// Question no 3
+// var input = prompt("Enter a Word")
+// var letter = prompt("Which letter you want to choose")
+// var Index = input.indexOf(letter)
+// document.write("String : " + input)
+// document.write("<br>Index of letter '" + letter+ "' : " + Index )
+
+// Question no 4
+// var input = "Hello world"
+// var letter = "l"
+// var index = input.lastIndexOf(letter)
+// document.write("String : " + input)
+// document.write("Last index of 'l' : " + index)
+
+// Question no 5
+// var input = "Pakistani"
+// var index = input.charAt(7)
+// document.write("String : " + input)
+// document.write("<br>Character at index '7' : " + index)
+
+// Question no 6
+// var first_name = prompt("Enter your First Name")
+// var last_name = prompt("Enter your Last Name")
+
+// var firstChar = first_name.slice(0, 1);
+// var otherChars = first_name.slice(1);
+// firstChar = firstChar.toUpperCase();
+// otherChars = otherChars.toLowerCase();
+// var changed_firstname = firstChar + otherChars;
+
+// var firstChar2 = last_name.slice(0, 1);
+// var otherChars2 = last_name.slice(1);
+// firstChar2 = firstChar2.toUpperCase();
+// otherChars2 = otherChars2.toLowerCase();
+// var changed_lastname = firstChar2 + otherChars2;
+
+// alert("Hi! " + changed_firstname + " " + changed_lastname)
+
+// Question no 7
+// var text = "Hyderabad"
+// var new_text = text.replace("Hyder","Islam")
+// document.write("City : " + text)
+// document.write("<br>After Replacement : " + new_text)
+
+// Question no 8
